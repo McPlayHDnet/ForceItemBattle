8f14679aea6df43fb0d96034b15d049eb8460e36
realthreeseconds <threeeseconds@gmail.com>
2024-01-27 16:37:59 +0100

    updated infowiki command

--- src/main/java/forceitembattle/commands/CommandInfoWiki.java
+++ src/main/java/forceitembattle/commands/CommandInfoWiki.java
@@ -9,6 +9,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class CommandInfoWiki implements CommandExecutor {
 
@@ -16,15 +17,24 @@ public class CommandInfoWiki implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         if (!(commandSender instanceof Player player)) return false;
 
-        if(ForceItemBattle.getTimer().isRunning()) {
-            Material currentMaterial = ForceItemBattle.getGamemanager().getCurrentMaterial(player);
-
-            TextComponent infoWikiBefore = new TextComponent("§7Check out the minecraft wiki for §a" + WordUtils.capitalize(currentMaterial.name().toLowerCase().replace("_", " ") + " "));
-            TextComponent infoWikiAfter = new TextComponent("§f[§bClick here§f]");
-            infoWikiAfter.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://minecraft.wiki/" + currentMaterial.name().toLowerCase()));
-
-            player.spigot().sendMessage(infoWikiBefore, infoWikiAfter);
+        ItemStack item;
+        if (ForceItemBattle.getTimer().isRunning()) {
+            item = new ItemStack(ForceItemBattle.getGamemanager().getCurrentMaterial(player));
+        } else {
+            item = player.getInventory().getItemInMainHand();
         }
+
+        if (item.getType() == Material.AIR) {
+            player.sendMessage("§cYou need to hold an item in your hand!");
+            return false;
+        }
+
+        TextComponent infoWikiBefore = new TextComponent("§7Check out the minecraft wiki for §a" + WordUtils.capitalize(item.getType().name().toLowerCase().replace("_", " ") + " "));
+        TextComponent infoWikiAfter = new TextComponent("§f[§bClick here§f]");
+        infoWikiAfter.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://minecraft.wiki/" + item.getType().name().toLowerCase()));
+
+        player.spigot().sendMessage(infoWikiBefore, infoWikiAfter);
+
         return false;
     }
 }
