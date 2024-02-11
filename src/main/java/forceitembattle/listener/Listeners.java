0e5a497ad6928334cd24105c05800eca27c2531d
realthreeseconds <threeeseconds@gmail.com>
2024-02-11 02:02:26 +0100

    fixed gamebreaking bug #48

--- src/main/java/forceitembattle/listener/Listeners.java
+++ src/main/java/forceitembattle/listener/Listeners.java
@@ -25,13 +25,9 @@ import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.inventory.InventoryAction;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.*;
 import org.bukkit.event.player.*;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.*;
 
 import java.util.ArrayList;
 
@@ -106,6 +102,11 @@ public class Listeners implements Listener {
         if (!this.plugin.getGamemanager().isMidGame()) {
             return;
         }
+
+        if(inventoryClickEvent.getClickedInventory() instanceof CraftingInventory ||
+                inventoryClickEvent.getClickedInventory() instanceof SmithingInventory ||
+                inventoryClickEvent.getClickedInventory() instanceof FurnaceInventory ||
+                inventoryClickEvent.getClickedInventory() instanceof BrewerInventory) return;
 
         ForceItemPlayer forceItemPlayer = this.plugin.getGamemanager().getForceItemPlayer(player.getUniqueId());
         ItemStack clickedItem = inventoryClickEvent.getCurrentItem();
@@ -256,8 +257,6 @@ public class Listeners implements Listener {
 
         if(clickedItem == null) return;
 
-        player.sendMessage(clickedItem.getType().name());
-
         if (clickedItem.getType() == currentItem) {
             FoundItemEvent foundItemEvent = new FoundItemEvent(player);
             foundItemEvent.setFoundItem(clickedItem);
@@ -281,8 +280,6 @@ public class Listeners implements Listener {
 
         if(clickedItem == null) return;
 
-        player.sendMessage(clickedItem.toString());
-
         if (clickedItem.getType() == currentItem) {
             FoundItemEvent foundItemEvent = new FoundItemEvent(player);
             foundItemEvent.setFoundItem(clickedItem);
@@ -304,8 +301,6 @@ public class Listeners implements Listener {
         ItemStack clickedItem = playerBucketEntityEvent.getOriginalBucket();
         Material currentItem = forceItemPlayer.currentMaterial();
 
-        player.sendMessage(clickedItem.toString());
-
         if (clickedItem.getType() == currentItem) {
             FoundItemEvent foundItemEvent = new FoundItemEvent(player);
             foundItemEvent.setFoundItem(clickedItem);
@@ -313,6 +308,33 @@ public class Listeners implements Listener {
 
             Bukkit.getPluginManager().callEvent(foundItemEvent);
         }
+    }
+
+    @EventHandler
+    public void onCrafting(CraftItemEvent craftItemEvent) {
+        Player player = (Player) craftItemEvent.getWhoClicked();
+
+        if (!this.plugin.getGamemanager().isMidGame()) {
+            return;
+        }
+
+        if(craftItemEvent.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY || craftItemEvent.getAction() == InventoryAction.PICKUP_ALL) {
+            ForceItemPlayer forceItemPlayer = this.plugin.getGamemanager().getForceItemPlayer(player.getUniqueId());
+            ItemStack clickedItem = craftItemEvent.getCurrentItem();
+            Material currentItem = forceItemPlayer.currentMaterial();
+
+            if(clickedItem == null) return;
+
+            if (clickedItem.getType() == currentItem) {
+                FoundItemEvent foundItemEvent = new FoundItemEvent(player);
+                foundItemEvent.setFoundItem(clickedItem);
+                foundItemEvent.skipped(false);
+
+                Bukkit.getPluginManager().callEvent(foundItemEvent);
+            }
+        }
+
+
     }
 
     /* Click-Event for my inventory builder */
