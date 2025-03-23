a536c259b8f357d25881215475a2eee556cd3498
realthreeseconds <threeeseconds@gmail.com>
2025-03-23 23:07:29 +0100

    Revert "added recipe view for colored bundles"
    This reverts commit 2b0ecbed8f1650ba25b84222d8e1ffbe3072fbb9.

--- src/main/java/forceitembattle/util/RecipeInventory.java
+++ src/main/java/forceitembattle/util/RecipeInventory.java
@@ -270,12 +270,6 @@ public class RecipeInventory extends InventoryBuilder {
                 break;
             }
 
-            if (material.name().contains("BUNDLE")) {
-                lore.clear();
-                lore.add(" <dark_gray>» <dark_aqua>any bundle");
-                break;
-            }
-
             if (recipeViewer.itemStack().getType() == Material.SMOKER || recipeViewer.itemStack().getType() == Material.CAMPFIRE || recipeViewer.itemStack().getType() == Material.SOUL_CAMPFIRE || recipeViewer.itemStack().getType() == Material.CHARCOAL) {
                 lore.clear();
                 lore.add(" <dark_gray>» <dark_aqua>any wooden log/wood (and stripped variants)");
@@ -303,7 +297,6 @@ public class RecipeInventory extends InventoryBuilder {
                 lore.add(" <dark_gray>» <dark_aqua>any leaves");
                 break;
             }
-
         }
 
         itemBuilder.setLore(lore);
