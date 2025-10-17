d8e8e7a61227b3262132814b1b4a7c9292834c9d
realthreeseconds <threeeseconds@gmail.com>
2025-10-17 16:13:45 +0200

    added shout toggle

--- src/main/java/forceitembattle/commands/player/CommandShout.java
+++ src/main/java/forceitembattle/commands/player/CommandShout.java
@@ -4,7 +4,12 @@ import forceitembattle.commands.CustomCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CommandShout extends CustomCommand {
+
+    private static final Set<Player> shoutingPlayers = new HashSet<>();
 
     public CommandShout() {
         super("shout");
@@ -13,7 +18,24 @@ public class CommandShout extends CustomCommand {
 
     @Override
     public void onPlayerCommand(Player player, String label, String[] args) {
-        Bukkit.broadcast(this.plugin.getGamemanager().getMiniMessage().deserialize("<gold>" + player.getName() + " <dark_gray>» <white>" + String.join(" ", args)));
+        if (args.length == 0) {
+            if (shoutingPlayers.contains(player)) {
+                shoutingPlayers.remove(player);
+                player.sendMessage(this.plugin.getGamemanager().getMiniMessage().deserialize("<gray>Shout mode: <red>OFF"));
+            } else {
+                shoutingPlayers.add(player);
+                player.sendMessage(this.plugin.getGamemanager().getMiniMessage().deserialize("<gray>Shout mode: <green>ON"));
+            }
+            return;
+        }
 
+        // Just in case, if someone wants to one-time shout
+        Bukkit.broadcast(this.plugin.getGamemanager().getMiniMessage().deserialize(
+                "<gold>" + player.getName() + " <dark_gray>» <white>" + String.join(" ", args)
+        ));
+    }
+
+    public static boolean isShouting(Player player) {
+        return shoutingPlayers.contains(player);
     }
 }
