2668801eb1ced206b8fb42d331b7418b9f9f8c05
eltobito <eltobito@duck.com>
2026-07-07 20:47:42 +0200

    bumped service version

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ repositories {
 dependencies {
     paperweight.paperDevBundle("26.2.build.+")
     implementation("org.apache.commons:commons-text:1.13.1")
-    implementation("de.threeseconds:FIBServiceClient:0.0.4")
+    implementation("de.threeseconds:FIBServiceClient:0.0.5")
     // paperweight.foliaDevBundle("1.20.4-R0.1-SNAPSHOT")
     // paperweight.devBundle("com.example.paperfork", "1.20.4-R0.1-SNAPSHOT")
 }
