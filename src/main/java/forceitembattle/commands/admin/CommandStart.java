fe0bec60d2083e0756b7e11cd1f583b6ce2ee717
eltobito <eltobito@duck.com>
2026-07-14 14:45:10 +0200

    blyat

--- src/main/java/forceitembattle/commands/admin/CommandStart.java
+++ src/main/java/forceitembattle/commands/admin/CommandStart.java
@@ -81,7 +81,7 @@ public class CommandStart extends CustomCommand implements CustomTabCompleter {
         }
 
         if (this.plugin.getSettings().isSettingEnabled(GameSetting.TEAM)) {
-            if (plugin.getGamemanager().forceItemPlayerMap().size() < 1) {
+            if (plugin.getGamemanager().forceItemPlayerMap().size() < 4) {
                 Bukkit.broadcast(Text.of("<red>There are not enough players online to enable teams"));
                 this.plugin.getSettings().setSettingEnabled(GameSetting.TEAM, false);
                 this.plugin.getTeamManager().clearAllTeams();
